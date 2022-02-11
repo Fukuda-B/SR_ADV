e2360f09e8aef2fdf00ccf4dfe495b856365bd02
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2022-02-12 03:08:43 +0900

    Update make_d.py

--- make_d.py
+++ make_d.py
@@ -19,7 +19,8 @@ settings = {
     'seed' : 1011,
     'random_crop_cnt' : 8, # Number of times to crop from a single image
     'crop_size' : (128, 128),
-    'img_format' : 'png',
+    'load_img_format' : 'png',
+    'save_img_format' : 'jpg',
 }
 
 # -----
@@ -31,9 +32,12 @@ def random_crop(image_p, crop_size):
     # print(type(image_p))
     image = cv2.imread(image_p)
     h, w, c = image.shape
+    if (h < crop_size[0] or w < crop_size[1]):
+        raise Exception('image is smaller then the crop size')
     pos_top = np.random.randint(0, h-crop_size[0])
     pos_left = np.random.randint(0, h-crop_size[1])
-    return image[pos_top:pos_top+crop_size[0], pos_left:pos_left+crop_size[1]]
+    res = image[pos_top:pos_top+crop_size[0], pos_left:pos_left+crop_size[1]]
+    return res
 
 def run_crop(settings):
     '''
@@ -41,13 +45,16 @@ def run_crop(settings):
     '''
     load_path = Path(settings['image_dir_load'])
     save_path = Path(settings['image_dir_save'])
-    img_f = list(load_path.glob('*.'+settings['img_format']))
+    img_f = list(load_path.glob('*.'+settings['load_img_format']))
     cc = 0
     for i in tqdm(img_f):
         i = str(i)
         for j in range(settings['random_crop_cnt']):
-            proc_img = random_crop(i, settings['crop_size'])
-            save_name = f"{cc}.{settings['img_format']}"
+            try:
+                proc_img = random_crop(i, settings['crop_size'])
+            except Exception as e:
+                continue
+            save_name = f"{cc}.{settings['save_img_format']}"
             s_path = save_path.joinpath(save_name)
             s_path = str(s_path)
             cv2.imwrite(s_path, proc_img)
