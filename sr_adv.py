0d4f305e268a8845962c050a92136991ab1aa67c
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2022-02-18 15:13:58 +0900

    うああああああああああああああああああ

--- sr_adv.py
+++ sr_adv.py
@@ -1,15 +1,18 @@
 '''
     SR_ADV | sr_adv.py
+
+    generate images using weights
 '''
 
 # ---- module
 import os
 import torch
-from torch.autograd import Variable
 from pathlib import Path
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.autograd import Variable
 from train_set import TestImageDataset
+from torchvision.utils import save_image
 
 # ----- config
 import model
@@ -19,34 +22,34 @@ import train
 opt = train.Param()
 
 # ----- main
-generator_weight_path = 'G:/IMG_Dataset/weight/generator_00001000_pre.pth'
+generator_weight_path = 'G:/IMG_Dataset/weight/generator_00008000.pth'
 
 def denormalize(t):
     for i in range(3):
         t[:, i].mul_(opt.std[i]).add_(opt.mean[i])
     return torch.clamp(t, 0, 255)
 
-demo_dataloader = DataLoader(
-    TestImageDataset(settings.image_dir_demo),
-    batch_size=1,
-    shuffle=False,
-    num_workers=opt.num_workers,
-)
+# ----- test
 
-generator = model.GeneratorRRDB(opt.channels, filters=64, num_res_blocks=opt.residual_blocks).to(opt.device)
-generator.load_state_dict(torch.load(generator_weight_path))
-Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
+if __name__ == '__main__':
+    demo_dataloader = DataLoader(
+        TestImageDataset(settings.image_dir_demo), batch_size=1, shuffle=False, num_workers=opt.num_workers,)
+    generator = model.GeneratorRRDB(opt.channels, filters=64, num_res_blocks=opt.residual_blocks).to(opt.device)
+    generator.load_state_dict(torch.load(generator_weight_path))
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
 
-with torch.no_grad():
-    for i, imgs in enumerate(demo_dataloader):
-        imgs_lr = Variable(imgs['lr'].type(Tensor))
+    # print(type(demo_dataloader))
+    with torch.no_grad():
+        for i, imgs in enumerate(demo_dataloader):
+            print(i)
+            imgs_lr = Variable(imgs['lr'].type(Tensor))
 
-        gen_hr = generator(imgs_lr)
-        imgs_lr = nn.functional.interpolate(imgs_lr, scale_factor=4)
+            gen_hr = generator(imgs_lr)
+            imgs_lr = nn.functional.interpolate(imgs_lr, scale_factor=4)
 
-        imgs_lr = denormalize(imgs_lr)
-        gen_hr = denormalize(gen_hr)
-        os.mkdirs(settings.image_dir_demo, exist_ok=True)
+            imgs_lr = denormalize(imgs_lr)
+            gen_hr = denormalize(gen_hr)
+            os.makedirs(settings.image_dir_demo, exist_ok=True)
 
-        model.save_tmp_image(imgs_lr, Path(settings.image_dir_demo).joinpath('low_{:01}.{}'.format(i, settings.demo_img_format), nrow=1, normalize=False))
-        model.save_tmp_image(gen_hr, Path(settings.image_dir_demo).joinpath('gen_hr_{:01}.{}'.format(i, settings.demo_img_format), nrow=1, normalize=False))
+            save_image(imgs_lr, Path(settings.image_dir_demo).joinpath('low_{:01}.{}'.format(i, settings.demo_img_format)), normalize=False)
+            save_image(gen_hr, Path(settings.image_dir_demo).joinpath('gen_hr_{:01}.{}'.format(i, settings.demo_img_format)), normalize=False)
